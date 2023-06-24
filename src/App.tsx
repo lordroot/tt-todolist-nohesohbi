6d174e1a31dbbc39cbab81c5d99473876ead1a32
lordroot <sohbi.nohe@gmail.com>
2023-06-24 14:32:27 +0200

    Working ToDo list with button clickable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,79 @@
 import React, {useState} from 'react';
 import './App.css';
-import TaskList from './TaskList'
+import { useFetch } from './custom-hook/useFetch'
+
+const url = `https://my-json-server.typicode.com/groupevaleco/tt-fake-todolist/tasks`
+
+interface TaskItem 
+{
+  id: number
+  title: string
+  completed: boolean
+}
+
+function TaskItem({ task }: { task: TaskItem }) 
+{
+  const [taskState, setTaskState] = useState<TaskItem>(task);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskState((prevTaskState) => ({
+      ...prevTaskState,
+      completed: !prevTaskState.completed
+    }));
+  };
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id={taskState.id.toString()}
+        name={taskState.title}
+        checked={taskState.completed}
+        onChange={handleChange}
+      />
+      <label htmlFor={taskState.id.toString()}>{taskState.title}</label>
+    </div>
+  );
+}
+
+
+function TaskList() 
+{
+    //const [taskItem, setTaskItem] = useState<TaskItem[]>([]); // { id: 0, title: '', completed: false }
+    const { data, error } = useFetch<TaskItem[]>(url);
+
+    if (error) {
+        return (
+            <p>An error occurred</p>
+        )
+    }
+
+    if (!data) {
+        return (
+            <p>Loading data...</p>
+        )
+    }
+
+    return (
+        <>
+            {
+                data.map(
+                    (task) => {return <TaskItem task={task}/>;}
+                )
+            }
+        </>
+    )
+}
 
 function App() {
-  const [taskItem, setTaskItem] = useState()
 
   return (
     <div className="App">
       <header className="App-header">
-        <TaskList />
+        <fieldset>
+          <legend>All tasks to do :</legend>
+          {TaskList()}
+        </fieldset>
       </header>
     </div>
   );
